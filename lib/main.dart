32677a88015f7845f18ef023ed3744a30bd0ee99
Md. Al Assadulla <assad.it2003@gmail.com>
2025-11-14 02:38:56 +0600

    file

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
 
 
 
+
+
+import 'package:class6/testApp.dart';
 import 'package:flutter/material.dart';
 
-import 'fristscereen.dart';
-
 void main(){
-  runApp( MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Fasts(),
-  ));
+  runApp(Myapp());
+}
+
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      home: TestApp(),
+    );
+  }
 }
